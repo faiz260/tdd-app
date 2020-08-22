4bfd3614ea7f86646d16fd013b5f970c3f0d7821
faiz260 <59044640+faiz260@users.noreply.github.com>
2020-08-22 23:51:40 +0100

    Testing

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders hello world', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Hello world/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('find world in the document', ()=>{
+  const renderResponse = render(<App/>);
+  const worldText = renderResponse.getByText(/world/i);
+  expect(worldText).toBeInTheDocument();
+})
